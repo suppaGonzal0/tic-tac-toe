80a1b593d6959ab466d13c255ae568fd835a0a00
Mehzabin Sadat Aothoi <mehzabin.sadat.aothoi@g.bracu.ac.bd>
2023-02-25 18:45:16 +0600

    Implemented winning logic and displayed winning text accordingly

--- script.js
+++ script.js
@@ -6,54 +6,44 @@ const winCombinations = [
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
-    [2, 4, 6]
-  ]
+    [2, 4, 6]]
 
 let playerTurn = true;
-
+let result = [];
 let availableCells = [0,1,2,3,4,5,6,7,8];
+let playerPositions = [];
+let cpuPositions = [];
 
-const positions = new Array(9).fill(0);
-
-const restartModal = document.getElementById("restartModal");
 const openRestartModal = document.getElementById("openRestart");
 const closeRestartModal = document.getElementById("cancelRestart");
-const bg = document.getElementById("bg");
-const cells = document.querySelectorAll("[cell]");
+const proceedRestart = document.getElementById("proceedRestart");
+const resultModal = document.getElementById("resultModalBg");
+const restartModal = document.getElementById("restartModalBg");
 
 openRestartModal.addEventListener('click', () => {
-    bg.style.visibility = "visible";
     restartModal.style.visibility = "visible"
 })
 
 closeRestartModal.addEventListener('click', () => {
-    bg.style.visibility = "hidden";
     restartModal.style.visibility = "hidden"
-    console.log(closeRestartModal.id);
 })
 
-
-// const startGame = () => {
-//     while(availableCells.length > 0){
-//         if(playerTurn){
-//             playerMove()
-//         } else{
-//             cpuMove()
-//         }        
-//         turn()    
-//     }
-// }
+proceedRestart.addEventListener('click', () => location.reload())
 
 const turn = () => {
     playerTurn = !playerTurn;
 }
 
 const playerMove = (id) => {
-        positions[id] = 1
+        playerPositions.push(id)
         availableCells = availableCells.filter(index => index != id)
-        if(availableCells.length > 0){
+        result = isWin()
+        if(availableCells.length > 0 && result[0] === "DRAW"){
             cpuMove()
+        } else{
+            showResultModal()
         }
+        document.getElementById(id).style.pointerEvents = "none"
 }
 
 function getRandomItem(arr) {
@@ -65,19 +55,48 @@ function getRandomItem(arr) {
 
 const cpuMove = () => {
     const cpuMove = getRandomItem(availableCells);
-    positions[cpuMove] = 9
+    cpuPositions.push(cpuMove)
     availableCells = availableCells.filter(index => index != cpuMove)
-    document.getElementById(Number(cpuMove)+10).innerText = "circle"
-    document.getElementById(cpuMove).checked = true;
-    console.log("CPU Turn", cpuMove)
+    const selectedCell = document.getElementById(cpuMove)
+    selectedCell.children[1].innerText = "circle"
+    selectedCell.children[1].style.color = "#F0B237"
+    selectedCell.children[0].checked = true;
+    selectedCell.style.pointerEvents = "none"
+    result = isWin()
+    if(result[0] !== "DRAW"){
+        showResultModal()
+    }
 }
 
 const isWin = () => {
-    
+    for(combination of winCombinations){
+        if(combination.every(value => {
+            return playerPositions.includes(value)
+        })){
+            return ["YOU WON!","X  TAKES THE ROUND"]
+        } else if(combination.every(value => {
+            return cpuPositions.includes(value)
+        })){
+            console.log("cpu")
+            return ["YOU LOST","O  TAKES THE ROUND"]
+        }
+    }
+    return ["DRAW", "NO ONE TAKES THE ROUND"]
 }
 
-const resetGrid = () => {
+// const resetGrid = () => {
+//     document.querySelectorAll('input[type=radio]').forEach((input) => {
+//         input.checked = false;
+//       });
+//     bg.style.visibility = "hidden";
+//     restartModal.style.visibility = "hidden"
+//     availableCells = [0,1,2,3,4,5,6,7,8];
+//     playerPositions = [];
+//     cpuPositions = [];
+// }
 
+const showResultModal = () => {
+    resultModal.style.visibility = "visible"
+    resultModal.children[0].children[0].innerText = result[0]
+    resultModal.children[0].children[1].innerText = result[1]
 }
-
-// startGame()
